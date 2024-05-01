96c64c978d47f8bd4cdbbf5747440ffcb57f3c83
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2024-05-02 00:25:41 +0200

    add video functionality

--- src/component/VideoGridItem.tsx
+++ src/component/VideoGridItem.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import { DurationFormats } from "../utils/DurationFormats";
 import { TimeFormatAgo } from "../utils/TimeFormatAgo";
 import { VIEW_FORMATTER } from "../utils/VIEW_FORMATTER";
@@ -27,16 +28,46 @@ const VideoGridItem = ({
   thumbnailUrl,
   videoUrl,
 }: VideoGridItemProps) => {
+  //STATES FOR PLAY VIDEOS
+  const [videoPlayed, setVideoPlayed] = React.useState(false);
+  const videoref = React.createRef<HTMLDivElement>();
+
+  useEffect(() => {
+    if (videoref.current == null) return;
+
+    if (videoPlayed) {
+      videoref.current.currentTime = 0;
+      videoref.current.play();
+    } else {
+      videoref.current.pause();
+    }
+  }, [videoPlayed, videoref]);
+
   return (
-    <div className="flex flex-col gap-2">
+    <div
+      className="flex flex-col gap-2"
+      onMouseEnter={() => setVideoPlayed(true)}
+      onMouseLeave={() => setVideoPlayed(false)}
+    >
       <a href={`/watch?tv=${id}`} className="relative aspect-video">
         <img
           src={thumbnailUrl}
-          className="block w-full h-full object-cover rounded-xl"
+          className={`block w-full h-full object-cover transition-[border-radius] duration-200 ${
+            videoPlayed ? "rounded-none" : "rounded-xl"
+          }`}
         />
         <div className="absolute bottom-1 right-1 bg-secondary-dark text-secondary text-sm px-0.5 rounded">
           {DurationFormats(duration)}
         </div>
+        <video
+          ref={videoref}
+          muted
+          playsInline
+          src={videoUrl}
+          className={`block h-full object-cover absolute inset-0 transition-opacity duration-200 ${
+            videoPlayed ? "opacity-100 delay-200" : "opacity-0"
+          }`}
+        />
       </a>
       <div className="flex gap-2 ">
         <a href={`/@${channel.id}`} className="flex-shrink-0">
